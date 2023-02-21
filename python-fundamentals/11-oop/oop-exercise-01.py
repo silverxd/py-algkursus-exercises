388bbb149f6dccabbe9a0a735c441fe8150843b3
silverxd <57298235+silverxd@users.noreply.github.com>
2023-02-21 10:18:33 +0200

    update

--- python-fundamentals/11-oop/oop-exercise-01.py
+++ python-fundamentals/11-oop/oop-exercise-01.py
@@ -1,3 +1,5 @@
+import time
+
 class Vehicle:
     
     name = ''
@@ -5,9 +7,45 @@ class Vehicle:
     color = ''
     value = ''
 
-    def description():
-        return self.name, self.type, self,color, self,value
+    def description(self):
+            return(f"Name: {self.name}, Type: {self.type}, Color: {self.color}, Value: {self.value}")
+
+    def __init__(self, name, value, color='undefined', type="undefined"):
+        self.color = color
+        self.type = type
+        self.name = name
+        self.value = value
+
+vehiclelist = []
+# car1 = Vehicle('suema', 'kallis', 'sinine', '4x4')
 
 
-    def __init__(self) -> None:
-        pass
+# car1.description()
+print('\nAutomüügiportaal Silver OÜ Esitleb:\nMüügiplats!\n')
+
+language = ["esimese", "teise", "kolmanda"]
+
+for i in range(3):
+	carname = input(f'Hakkame sisestama Teie {language[i]} auto andmeid! Alustuseks palun sisesta oma auto nimi: ')
+	carvalue = input('No palju sa sellise panni eest siis tahad? ')
+	
+	question = input('Kas soovid lisada ka värvi ja auto tüüpi? ')
+	if question.lower() == ('yes') or question.lower() == ('ok')  or question.lower() == ('okei') or question.lower() == ('okay') or question.lower() == ('jah') or question.lower() == ('ja'):
+		carcolor = input('Ja mis värvi su parsa siis on? ')
+		cartype = input('Kui ma täiesti aus olen siis mul pole aimugi, mis tüüpi masin see on. Äkki oskad aidata? ')
+
+	print('\nPalun oota, salvestan andmeid!\n')
+	# time.sleep(5)
+
+
+	try:
+		uusauto = Vehicle(carname, carvalue, carcolor, cartype)
+	except NameError:
+		uusauto = Vehicle(carname, carvalue)
+	
+	vehiclelist.append(uusauto.description())
+	# print(uusauto.description())
+	
+
+for i in range (len(vehiclelist)):
+	print(vehiclelist[i])
---
+++ python-fundamentals/12-fileops/easyex.py
@@ -0,0 +1,30 @@
+import string
+
+datadict = dict()
+wordcount = 0
+text = open ("//server/opilased$/Silver.Slinko/Documents/PYTON/12-fileops/data.txt", 'r')
+    
+for line in text:
+    line = line.strip()
+    line = line.lower()
+    line = line.translate(line.maketrans("", "", string.punctuation))
+    words = line.split(' ')
+
+    for word in words:
+        if word in datadict:
+            datadict[word] = datadict[word] + 1
+        else:
+            datadict[word] = 1
+        wordcount += 1
+
+with open('12-fileops/finaldat.txt', '+w') as f:
+    
+    f.write(f'Total word count: {wordcount}\n')
+    for key in list(datadict.keys()):
+        f.write(str(key) + ':' + str(datadict[key]) + "\n")
+
+    print(f'Total word count: {wordcount}\n')
+    for key in list(datadict.keys()):
+        print(str(key) + ':' + str(datadict[key]) + "\n")
+    
+    
