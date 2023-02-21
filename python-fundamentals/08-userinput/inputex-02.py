3a383aa06bb9b95debb7411091d7b14013bf6238
Silver school PC <keegisilver@gmail.com>
2023-02-21 16:05:30 +0200

    better naming

--- python-fundamentals/08-userinput/inputex-02.py
+++ python-fundamentals/08-userinput/inputex-02.py
@@ -1,19 +1,19 @@
 dict = {}
 def esc(code):
-    return f'\033[{code}m'
+    return f'\033[{code}m'  # colors
 
 for i in range(1,4):
     while True:
         try:
             print(f"Attempt{esc('31')} #{i} {esc('0')}")
-            funny = input(f"Sisesta riigi nimi ja pealinn, formaadis {esc('32')} riik, pealinn: {esc('0')}")
+            tempinput = input(f"Sisesta riigi nimi ja pealinn, formaadis {esc('32')} riik, pealinn: {esc('0')}")
     
-            asda=funny.split(sep=', ')
-            try:
-                dict [asda[0]] = asda[1]
+            output=tempinput.split(sep=', ')
+            try:    # real handling
+                dict [output[0]] = output[1]
             except IndexError:
-                    asda=funny.split(sep=',')
-                    dict [asda[0]] = asda[1]
+                    output=tempinput.split(sep=',')
+                    dict [output[0]] = output[1]
         except IndexError:
             print(f"{esc('31')}Invalid input!{esc('0')}")
             continue
