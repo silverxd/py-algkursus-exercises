415f183582318648e1c9b05e23f4ccf6c3222199
silverxd <57298235+silverxd@users.noreply.github.com>
2023-03-27 14:02:27 +0300

    skill issue

--- python-fundamentals/00-random-tests/sisseastumine.py
+++ python-fundamentals/00-random-tests/sisseastumine.py
@@ -3,12 +3,12 @@ def sisseastumine():
     a=int(input('Sisesta oma eesti keele eksami tulemus => '))
     b=int(input('Sisesta oma mate eksami tulemus => '))
 
-    if a <= 65 and b <= 75:
-        if a + b <= 150:
-            return('Oledki sees! Näeme TalTechis!')
+    if a >= 65 and b >= 75:
+        if a + b >= 150:
+            print('Oledki sees! Näeme TalTechis!')
         else:
-            return('Palju õnne, oled paigutatud pingeritta, oled võib-olla sees!')
+            print('Palju õnne, oled paigutatud pingeritta, oled võib-olla sees!')
     else:
-        return('Näeme TalTechi katsetel!')
+        print('Näeme TalTechi katsetel!')
     
 sisseastumine()
